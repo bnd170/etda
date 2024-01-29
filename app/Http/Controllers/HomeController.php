c2dbc7173dca62e8ec7b08db68b6f8d543cf01f3
Borja Pombo <borjapombo@gmail.com>
2024-01-29 20:28:37 +0100

    Fix ranking

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends Controller
         $nextGames  = Game::pending()->where('season_id', $season->id)->orderBy('date', 'asc')->limit(4)->get();
         $playedGames  = Game::notPending()->where('season_id', $season->id)->orderBy('date', 'desc')->limit(4)->get();
         $season  = Season::where('in_progress', true)->first();
-        $ranking = Ranking::where('season_id', $season->id)->orderBy('points', 'desc')->orderBy('losses', 'asc')->with(
+        $ranking = Ranking::where('season_id', $season->id)->orderBy('points', 'desc')->orderBy('goal_differential', 'desc')->with(
             'team'
         )->get();
         $news = News::orderBy('created_at', 'desc')->limit(3)->get();
