1e294d83fc9936eaae7302adc9026d3d280fd162
Borja <borjapombo@gmail.com>
2024-10-18 22:16:50 +0200

    Privacy policy

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -35,12 +35,14 @@ class RegisteredUserController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'privacy_policy_accepted' => 'required|accepted',
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'privacy_policy_accepted' => now(),
         ]);
 
         event(new Registered($user));
